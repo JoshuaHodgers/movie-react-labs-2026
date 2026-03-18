5d702e6fc185a4bafd99f64940c42280d8ee77dd
Joshua Hodgers <joshhodgers@gmail.com>
2026-03-18 20:21:37 +0000

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage'
+
+
 
 const sample = {
   adult: false,
--- movies/src/pages/movieDetailsPage.jsx
+++ movies/src/pages/movieDetailsPage.jsx
@@ -1,17 +1,71 @@
-import { getMovie } from '../api/tmdb-api'
-import { useQuery } from '@tanstack/react-query';
-import Spinner from '../components/spinner'
-// import useMovie from "../hooks/useMovie";   Redundant
+import React from "react";
+import MovieHeader from "../components/headerMovie/";
+import MovieDetails from "../components/movieDetails/";
+import Grid from "@mui/material/Grid";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { getMovie, getMovieImages } from "../api/tmdb-api";
+import { getGenres } from "../../api/tmdb-api";
 
-  const { data: movie, error, isPending, isError  } = useQuery({
-    queryKey: ['movie', {id: id}],
-    queryFn: getMovie,
-  })
+  useEffect(() => {
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
+  }, [id]);
+  useEffect(() => {
+    getGenres().then((allGenres) => {
+      setGenres([genres[0], ...allGenres]);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  if (isPending) {
-    return <Spinner />;
-  }
 
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
+const MoviePage = (props) => {
+  const movie = props.movie;
+  const images = props.images;
+
+  return (
+    <>
+      {movie ? (
+        <>
+          <MovieHeader movie={movie} />
+          <Grid container spacing={5} style={{ padding: "15px" }}>
+            <Grid size={{xs: 3}}>
+              <div sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+              }}>
+                <ImageList
+                  sx={{
+                    height: "100vh",
+                  }}
+                  cols={1}
+                >
+                  {images.map((image) => (
+                    <ImageListItem
+                      key={image}
+                      cols={1}
+                    >
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${image}`}
+                        alt={image}
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              </div>
+            </Grid>
+            <Grid size={{xs: 9}}>
+              <MovieDetails movie={movie} />
+            </Grid>
+          </Grid>
+        </>
+      ) : (
+        <h2>Waiting for API data</h2>
+      )}
+    </>
+  );
+};
+
+export default MoviePage;
