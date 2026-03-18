492d3a7607f0b73d246888595d98a6c71b9797c5
Joshua Hodgers <joshhodgers@gmail.com>
2026-03-18 21:34:56 +0000

    Refactor: Favourite movie list now stored in a context.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -12,6 +12,9 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import img from '../../images/film-poster-placeholder.png'
+import React, { useContext  } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+
 
 export default function MovieCard(props) {
   const movie = props.movie;
